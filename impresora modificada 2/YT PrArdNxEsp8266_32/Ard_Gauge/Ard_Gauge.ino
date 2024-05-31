8a48774b2de7acbc9c1d97be00cbaf990ad76d42
jh961 <83422509+jh961@users.noreply.github.com>
2024-05-30 23:26:26 -0500

    Add files via upload

---
+++ impresora modificada 2/YT PrArdNxEsp8266_32/Ard_Gauge/Ard_Gauge.ino
@@ -0,0 +1,28 @@
+
+#include "Nextion.h"
+
+NexGauge ObjGauge  = NexGauge(0, 1, "ObjGauge");
+
+byte pot = A0;
+
+void setup(void)
+{      
+    Serial.begin(115200);
+    nexInit();//Inicio la comunicacion entre el Arduino y la Pantalla Nextion
+    
+    ObjGauge.setValue(0);
+    Serial.println("Iniciando...");
+    delay(2000);
+}
+
+void loop(void)
+{     
+  int Aleatorio = analogRead(pot);//random(1,1023);
+  //Serial.println(Aleatorio + "/n");
+  //int Val_FunMap = map(Aleatorio, 1, 1023, 0, 360);
+  int Val_FunMap = map(Aleatorio, 1, 1023, 0, 215); 
+  Serial.print("Angulo: ");Serial.println(Val_FunMap);
+  
+  ObjGauge.setValue(Val_FunMap);
+  delay(500);
+}
