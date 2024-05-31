ec079cdec3e6b5d1549358952f5dec9e54fb2001
jh961 <83422509+jh961@users.noreply.github.com>
2024-05-30 23:38:05 -0500

    Add files via upload

---
+++ TACOMETRO/TACOMETRO.ino
@@ -0,0 +1,65 @@
+#include  <Wire.h>
+#include  <LiquidCrystal_I2C.h>
+
+
+const int pin=2;
+
+unsigned long contador =0;
+unsigned long tiempo;
+int Frecuencia =0;
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+
+void cuenta(){
+  contador++;
+}
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  
+  pinMode(pin,INPUT);
+
+  attachInterrupt(0, cuenta, RISING);
+
+  lcd.clear();
+  lcd.print("QUE MIRAA");
+  lcd.setCursor(0, 1);
+  lcd.print("BUENA LA RATA ");
+  delay(1000);
+
+  Serial.println("QUE COMIENCE EL JUEGO");
+  delay(100);
+}
+
+void loop() {
+  if((millis() - tiempo)>=979){
+
+    
+  lcd.clear();
+  
+  lcd.print("FRECUENCIA= ");
+  Serial.print("FRECUENCIA= ");
+  lcd.setCursor(0, 1);
+  contador = (contador/20)*60;
+  Serial.print(contador);
+  lcd.print(contador);
+  lcd.print(" RPM");
+  Serial.println(" RPM");
+
+  
+  Serial.print("tiempo: ");
+  tiempo = millis();
+  Serial.println(tiempo);
+  
+  contador=0;
+
+  
+  
+  }
+  
+}
+
+
+
---
+++ temperatura/temperatura.ino
@@ -0,0 +1,41 @@
+
+#include  <Wire.h>
+#include  <LiquidCrystal_I2C.h>
+ 
+
+
+float temperatura = 0;
+float valor=0;
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+ 
+//Definicion de caracteres personalizados
+
+void setup()
+{
+  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
+  lcd.begin(16, 2);
+  // Crear los caracteres personalizados en la pantalla
+
+  pinMode(A1,INPUT);
+
+  lcd.clear();  
+  lcd.print("AUTORES");
+  delay(1000);
+}
+
+void loop()
+{
+
+  valor=analogRead(A1);
+  temperatura=((0.125*valor)-15.98);
+  
+  lcd.clear(); 
+  lcd.print(temperatura);
+  lcd.print(" Temperatura");
+  delay(500);
+
+ }
+
+
