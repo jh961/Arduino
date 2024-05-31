6501f2761a91ef9d8812a9ea2d73507f5a9e3ac2
jh961 <83422509+jh961@users.noreply.github.com>
2024-05-30 23:33:11 -0500

    Add files via upload

---
+++ Motor_con_bluetooh/Motor_con_bluetooh.ino
@@ -0,0 +1,43 @@
+
+
+
+const int motord =2;
+const int motori =3;
+char caracter='.';
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(motord,OUTPUT);
+  pinMode(motori,OUTPUT);
+}
+
+void loop() {
+  while(Serial.available() > 0) {
+    // get incoming byte:
+    caracter = Serial.read();
+    Serial.println(caracter);
+  }
+
+  switch(caracter){
+    case 'd':    // your hand is on the sensor
+      analogWrite(motord,255);
+      analogWrite(motori,0);
+      Serial.println("Motor Derecha");
+      break;
+      
+    case 'b':    // your hand is on the sensor
+      analogWrite(motord,0);
+      analogWrite(motori,255);
+      Serial.println("Motor Izquierda");
+      break;
+      
+    case 'c':    // your hand is on the sensor
+      analogWrite(motord,0);
+      analogWrite(motori,0);
+      Serial.println("Motor Alto");
+      break;
+  }
+  caracter='.';
+}
---
+++ PROGRAMACION_ENCUBADORA/PROGRAMACION_ENCUBADORA.ino
@@ -0,0 +1,97 @@
+
+#include  <Wire.h>
+#include  <LiquidCrystal_I2C.h>
+#include "DHT.h"
+
+#define DHTPIN 2 //pin conexion sensor DH-11
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+const int espera=1000;
+const int espera1=2000;
+
+const int limtemp=30;
+const int limhumed=60;
+float h,t,f;
+
+const int bombillo =3;
+const int ventilador =4;
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+
+  lcd.begin(16, 2);
+  //lcd.home ();
+
+  pinMode(bombillo, OUTPUT);
+  pinMode(ventilador, OUTPUT);
+  
+  lcd.setCursor ( 0, 0 );
+  lcd.print("AUTOR");
+  lcd.setCursor ( 0, 1 );
+  lcd.print("JAIME DIAZ");
+  delay(espera);
+  
+  lcd.clear();
+  lcd.print("ENCUBADORA");
+  lcd.setCursor(0, 1);
+  lcd.print("V 1.0");
+  delay(espera);
+}
+ 
+void loop()
+{
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+
+  Serial.print("humedad  ");
+  Serial.println(h);
+  Serial.print("temperatura  ");
+  Serial.println(t);
+
+  if(h > limhumed){
+    digitalWrite(ventilador,HIGH);
+  }else if(h <= limhumed){
+    digitalWrite(ventilador,LOW);
+  }
+
+  if(t <= limtemp){
+    digitalWrite(bombillo,HIGH);
+  }else if(t > limtemp){
+    digitalWrite(bombillo,LOW);
+  }
+
+
+  lcd.setCursor ( 0, 0 );
+  lcd.print("humed y Temp");
+  lcd.setCursor ( 0, 1 );
+  lcd.print(h);
+  lcd.setCursor ( 7, 1 );
+  lcd.print(t);
+  delay(espera1);
+  
+ 
+}
+
+
+ /*
+  int i;
+ 
+  for ( int i = 0; i < 5; i++ ) {
+    lcd.scrollDisplayRight();
+    delay (1000);
+  }
+ 
+ 
+  for ( int i = 0; i < 5; i++ ) {
+    lcd.scrollDisplayLeft();
+    delay (1000);
+  }*/
